77e6be6267653911eac2a1f90d2e5eb122bdce7e
Chinmoy Paul <phpwebdeveloper29@gmail.com>
2021-07-11 16:41:49 +0530

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'ZionFfStyler\\Elements\\' => array($baseDir . '/elements'),
+    'ZionFfStyler\\Elements\\' => array($baseDir . '/Elements'),
     'ZionFfStyler\\' => array($baseDir . '/includes'),
 );
